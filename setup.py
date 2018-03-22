60e817226834d2a61cfa3d309c37185857ca4a32
Benjamin Kolligs <benkolligs@gmail.com>
2018-03-21 21:21:40 -0700

    adjusted url

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='renyi',
       version=0.1,
       description='RMI package',
-      url='https://github.com/bkolligs/entanglement-package.git',
+      url='https://github.com/bkolligs/renyi-package.git',
       author='Ben Kolligs',
       packages=['renyi'],
       install_requires= ['time', 'numpy', 'datetime', 'mpmath', 'pylab', 'random'],
